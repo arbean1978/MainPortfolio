0758492e84598f902135e95cee8f32cb49d61167
Aaron Bean <arbean1978@gmail.com>
2023-02-22 09:48:43 -0700

    about

--- components/About.jsx
+++ components/About.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/jsx-no-comment-textnodes */
 import Image from 'next/image'
 import React from 'react'
 import ProfileImage from '..//public/assets/profile.png.jpg'
@@ -10,7 +9,7 @@ const About = () => {
         <div className='col-span-2'>
           <p className='uppercase text-xl tracking-widest text-[#5651e5]'>About</p>
           <h2 className='py-4'>Who I Am</h2>
-          <p className='py-2 text-gray-600'>// I am not your normal developer</p>
+          <p className='py-2 text-gray-600'>I am not your normal developer</p>
           <p className='py-2 text-gray-600'>I have extensive experience in law enforcement and security but I have always been tech-savvy and enjoyed working wih computers.  In 2021 I decided to make a career change and enrolled in the University of Utah's full-stack web developer bootcamp.  After graduating I was quickly drawn to learn more.  I started learning things like Tailwindcss, NextJS, and others.  I now spend my free time building interactive websites based on my formal and individual education.</p>
           <p className='py-2 text-gray-600 underline cursor-pointer'>Check out some of my projects.</p>
         </div>
